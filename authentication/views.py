68f7013ffff4f4302c135b9c4b7f14bcd6678395
vparjunmohan <vparjunmohan@gmail.com>
2021-02-09 11:14:24 +0530

    ajax username validation added

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+import  json
+from django.http import  JsonResponse
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 
 
+class UsernameValidationView(View): 
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only conatin alphanumeric characters'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'Sorry, username already taken'}, status=409)
+        return JsonResponse({'username_valid': True})
 class RegistrationView(View): 
     def get(self, request):
         return render(request, 'authentication/register.html')
---
+++ expenseswebsite/static/js/register.js
@@ -0,0 +1,27 @@
+const usernameField=document.querySelector('#usernameField');
+const feedBackArea= document.querySelector('.invalid_feedback');
+
+
+
+usernameField.addEventListener('keyup', (e) => {console.log('7777', 7777);
+const usernameVal=e.target.value;
+
+usernameField.classList.remove("is-invalid");
+feedBackArea.style.display='none';
+
+if(usernameVal.length>0){
+    console.log('usernameVal', usernameVal);
+    fetch('/authentication/validate-username/',{
+        body:JSON.stringify({username: usernameVal}), 
+        method:'POST',
+    }).then(res=>res.json()).then(data=>{
+        console.log('data', data);
+        if(data.username_error){
+            usernameField.classList.add("is-invalid");
+            feedBackArea.style.display='block';
+            feedBackArea.innerHTML=`<p>${data.username_error}</p>`
+        }
+    });
+}
+
+})
