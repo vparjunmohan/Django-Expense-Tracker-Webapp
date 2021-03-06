e70727517efd293edb3491ebb012998cfc66c8b9
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2021-03-06 10:41:54 +0530

    added expense create functionality

--- expenses/views.py
+++ expenses/views.py
@@ -1,7 +1,8 @@
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-
+from .models import Category, Expense
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,8 +10,32 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/authentication/login/')
 def index(request):
+    categories = Category.objects.all()
+
     return render(request, 'expenses/index.html')
 
 
 def add_expense(request):
-    return render(request, 'expenses/add_expenses.html')
+    categories = Category.objects.all()
+    context = {'categories': categories, 'values':request.POST}
+    if request.method=='GET':
+        return render(request, 'expenses/add_expenses.html', context)
+
+    if request.method=='POST':
+        amount = request.POST['amount']
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/add_expenses.html', context)
+    
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+        if not description:
+            messages.error(request, 'Description is required')
+            return render(request, 'expenses/add_expenses.html', context)
+
+        Expense.objects.create(owner=request.user, amount=amount, date=date, description=description, category=category)
+
+        messages.success(request, 'Expenses added')
+
+        return redirect('expenses')
