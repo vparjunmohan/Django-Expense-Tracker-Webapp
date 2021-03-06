009ebb676e9a3fc84d9c66f626364123abaf9d85
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2021-03-06 11:40:45 +0530

    changed settings.py, updated gitignore

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -86,11 +86,8 @@ WSGI_APPLICATION = 'expenseswebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'incomeexpensesdb',
-        'USER': 'postgres',
-        'PASSWORD': 'abcd123#',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- userpreferences/admin.py
+++ userpreferences/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserPreference
 
 # Register your models here.
+
+admin.site.register(UserPreference)
