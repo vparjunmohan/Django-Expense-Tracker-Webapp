5db985f25c80df89bc5344a91fc9d80670f4af3a
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2021-03-08 19:07:09 +0530

    implemented Ajax search option

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,10 +1,12 @@
 from .views import *
 from django.urls import  path
-
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', index, name='expenses'),
     path('add-expense/', add_expense, name='add-expenses'),
     path('edit-expense/<int:pk>', expense_edit, name='expense-edit'),
     path('delete-expense/<int:pk>', delete_expense, name='expense-delete'),
+    path('search-expenses', csrf_exempt(search_expenses), name='search_expenses'),
+
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -5,9 +5,18 @@ from .models import Category, Expense
 from django.contrib import messages
 
 from django.core.paginator import Paginator
+import json
+from django.http import JsonResponse
 # Create your views here.
 
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
 
+        expenses =  Expense.objects.filter(amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(date__istartswith=search_str, owner=request.user) | Expense.objects.filter(description__istartswith=search_str, owner=request.user) | Expense.objects.filter(amount__icontains=search_str, owner=request.user) | Expense.objects.filter(category__icontains=search_str, owner=request.user)
+    
+    data = expenses.values()
+    return JsonResponse(list(data), safe=False)
 
 @login_required(login_url='/authentication/login/')
 def index(request):
